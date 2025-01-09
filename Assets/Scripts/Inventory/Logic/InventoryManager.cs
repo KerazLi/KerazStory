600f341108bd2f01788c5cf0bae638d3736adf95
Keraz <1530217461@qq.com>
2025-01-10 04:24:52 +0800

    实现背包的数据结构

--- Assets/Scripts/Inventory/Logic/InventoryManager.cs
+++ Assets/Scripts/Inventory/Logic/InventoryManager.cs
@@ -4,11 +4,11 @@ namespace Inventory.Logic
 {
     /// <summary>
     /// 背包物品管理函数
-    /// 
     /// </summary>
     public class InventoryManager : Singleton<InventoryManager>
     {
         public ItemDataList_SO itemDataList_SO;
+        public InventoryBag_SO playerBag;
         
         public ItemDetails GetItemDetails(int ID)
         {
@@ -16,6 +16,12 @@ namespace Inventory.Logic
         }
         public void AddItem(Item item,bool toDestory)
         {
+            //背包是否有空位
+            //是否已经有该物品
+            InventoryItem newItem = new();
+            newItem.itemID = item.itemID;
+            newItem.ItemAmount = 1;
+            playerBag.itemList[0] = newItem;
             Debug.Log(GetItemDetails(item.itemID).itemID+"Name"+GetItemDetails(item.itemID).itemName);
             if (toDestory)
             {
